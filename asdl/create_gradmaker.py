f15599a982ca294c6103179f902de85f66432f5f
riverstone496 <robosatoki@outlook.jp>
2023-05-13 16:57:08 +0900

    first commit

---
+++ asdl/create_gradmaker.py
@@ -0,0 +1,72 @@
+import torch
+from torch import nn
+import torch_optimizer as optim
+from .precondition import *
+
+OPTIM_SGD = 'sgd'
+OPTIM_ADAM = 'adam'
+OPTIM_ADAMW = 'adamw'
+OPTIM_SHAMPOO='shampoo'
+OPTIM_KFAC_MC = 'kfac_mc'
+OPTIM_KFAC_EMP = 'kfac_emp'
+OPTIM_NOISY_KFAC_MC = 'noisy_kfac_mc'
+OPTIM_SMW_NGD = 'smw_ngd'
+OPTIM_FULL_PSGD = 'full_psgd'
+OPTIM_KRON_PSGD = 'psgd'
+OPTIM_NEWTON = 'newton'
+OPTIM_ABS_NEWTON = 'abs_newton'
+OPTIM_KBFGS = 'kbfgs'
+OPTIM_CURVE_BALL = 'curve_ball'
+OPTIM_SENG = 'seng'
+OPTIM_ADAHESSIAN = 'adahessian'
+OPTIM_SWATS = 'swats'
+OPTIM_FOOF = 'foof'
+OPTIM_BOOB = 'boob'
+OPTIM_NGD_LAYER_WISE = 'ngd_layerwise'
+OPTIM_NGD_FULL = 'ngd_full'
+OPTIM_LARS = 'lars'
+OPTIM_ADAM_ASDL = 'adam_asdl'
+
+def create_grad_maker(model,args, optimizer=None):
+    config = PreconditioningConfig(data_size=args.batch_size,
+                                    damping=args.damping,
+                                    ema_decay = args.ema_decay,
+                                    preconditioner_upd_interval=args.curvature_update_interval,
+                                    curvature_upd_interval=args.curvature_update_interval,
+                                    ignore_modules=[nn.BatchNorm1d,nn.BatchNorm2d,nn.BatchNorm3d,nn.LayerNorm],
+                                    block_size=args.block_size,
+                                    grafting=args.grafting
+                                    )
+
+    if args.optim == OPTIM_KFAC_MC:
+        grad_maker = KfacGradientMaker(model, config, eye_initialization = args.eye_initialization)
+    elif args.optim == OPTIM_KFAC_EMP:
+        grad_maker = KfacEmpGradientMaker(model, config, eye_initialization = args.eye_initialization)
+    elif args.optim == OPTIM_ADAM_ASDL:
+        grad_maker = AdamGradientMaker(model, config, optimizer=optimizer, grad_unit=args.grad_unit, before_momentum = args.before_momentum)
+    elif args.optim == OPTIM_NGD_FULL:
+        grad_maker = FullNaturalGradientMaker(model, config)
+    elif args.optim == OPTIM_NGD_LAYER_WISE:
+        grad_maker = LayerWiseNaturalGradientMaker(model, config)
+    elif args.optim == OPTIM_SHAMPOO:
+        grad_maker = ShampooGradientMaker(model,config)
+    elif args.optim == OPTIM_SMW_NGD:
+        grad_maker = SmwEmpNaturalGradientMaker(model, config)
+    elif args.optim == OPTIM_FULL_PSGD:
+        grad_maker = PsgdGradientMaker(model)
+    elif args.optim == OPTIM_KRON_PSGD:
+        grad_maker = KronPsgdGradientMaker(model,config)
+    elif args.optim == OPTIM_NEWTON:
+        grad_maker = NewtonGradientMaker(model, config)
+    elif args.optim == OPTIM_ABS_NEWTON:
+        grad_maker = NewtonGradientMaker(model, config)
+    elif args.optim == OPTIM_KBFGS:
+        grad_maker = KronBfgsGradientMaker(model, config)
+    elif args.optim == OPTIM_CURVE_BALL:
+        grad_maker = CurveBallGradientMaker(model, config)
+    elif args.optim == OPTIM_SENG:
+        grad_maker = SengGradientMaker(model,config=config)
+    else:
+        grad_maker = PreconditionedGradientMaker(model,config)
+
+    return grad_maker
---
+++ asdl/precondition/adam.py
@@ -0,0 +1,58 @@
+import torch
+import math
+from .prec_grad_maker import PreconditionedGradientMaker, PreconditioningConfig
+import torch.nn as nn
+
+class AdamGradientMaker(PreconditionedGradientMaker):
+    """
+    implements ADAM Algorithm, as a preceding step.
+    """
+    def __init__(self, model: nn.Module, config, optimizer):
+        super().__init__(model, config)
+        self.optim = optimizer
+        self.eps = config.damping
+        self.momentum = 0
+        self.beta2 = 1-config.ema_decay
+
+    @torch.no_grad()
+    def update_curvature(self):
+        """
+        Performs a single optimization step.
+        """
+        loss = None
+        for (name, module) in self.module_dict.items():
+            if module.weight.grad is not None:
+                grad = module.weight.grad
+                if not hasattr(module.weight,'exp_avg'):
+                    module.weight.exp_avg = torch.zeros_like(module.weight.grad)
+                exp_avg_sq = module.weight.exp_avg
+                # RMS
+                exp_avg_sq = torch.mul(exp_avg_sq, self.beta2) + (1-self.beta2)*(grad*grad)
+                module.weight.exp_avg = exp_avg_sq
+            if hasattr(module,'exp_avg') and module.bias.grad is not None:
+                grad = module.bias.grad
+                if not hasattr(module.bias,'exp_avg'):
+                    module.bias.exp_avg = torch.zeros_like(module.bias.grad)
+                exp_avg_sq = module.bias.exp_avg
+                # RMS
+                exp_avg_sq = torch.mul(exp_avg_sq, self.beta2) + (1-self.beta2)*(grad*grad)
+                module.bias.exp_avg = exp_avg_sq
+                denom = exp_avg_sq.sqrt() + self.eps
+                module.bias.curvature = denom 
+
+    @torch.no_grad()
+    def precondition(self):
+        """
+        Performs a single optimization step.
+        """
+        loss = None
+        for (name, module) in self.module_dict.items():
+            if module.weight.grad is not None:
+                grad = module.weight.grad
+                denom = module.weight.exp_avg.sqrt() + self.eps
+                module.weight.grad = grad / denom
+            if hasattr(module,'exp_avg') and module.bias.grad is not None:
+                grad = module.bias.grad
+                denom = module.bias.exp_avg.sqrt() + self.eps
+                module.bias.grad = grad / denom  
+        return loss
